0f7991c4043bfa60ad53dd9fca4502dfe24bff9b
rajifmahendra <rajifmahendra10@gmail.com>
2019-12-27 00:20:09 +0700

    finish validasi and login multiuser

--- routes/web.php
+++ routes/web.php
@@ -23,4 +23,4 @@ Route::prefix('admin')->namespace('Admin')->middleware(['auth', 'admin'])->group
   Route::get('/', 'DashboardController@index')->name('dashboard');
 });
 
-Auth::routes();
+Auth::routes(['verify' => true]);
